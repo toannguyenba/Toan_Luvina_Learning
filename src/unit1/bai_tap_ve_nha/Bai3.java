5834a9e45191d4aaf5b7aca013531825c4a2d522
Nguyen Ba Toan <ngabatoan22@gmail.com>
2020-09-23 10:17:55 +0700

    lam bai 3

--- src/unit1/bai_tap_ve_nha/Bai3.java
+++ src/unit1/bai_tap_ve_nha/Bai3.java
@@ -4,13 +4,22 @@ public class Bai3 {
 
 //	3. Cho một giá trị int x=7 và một mảng  int [] A ={ 4, 6, 9, 10, 17, 22}; gồm các phần tử tạo thành 1 dãy tăng dần                     
 //	Hãy bỏ phần tử cuối cùng của mảng A và chèn x vào mảng A để dãy kết quả nhận được là một dãy tăng dần.
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
 
-		int[] A = { 4, 6, 9, 10, 17, 22 };
-		for (int i = A.length - 1; i >= 0; i++) {
-			
-		}
-	}
-
+        int[] A = {4, 6, 9, 10, 17, 22};
+        int x = 7;
+        A[A.length - 1] = x;
+        for (int i = A.length - 1; i > 0; i--) {
+            if (x > A[i-1]) {
+                break;
+            } else {
+                A[i] = A[i-1];
+                A[i-1] = x;
+            }
+        }
+        for(int i = 0; i < A.length; i++) {
+            System.out.print(A[i] + " ");
+        }
+    }
 }
---
+++ src/unit1/bai_tap_ve_nha/Bai4.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unit1.bai_tap_ve_nha;
+
+/**
+ *
+ * @author LA-AM
+ */
+public class Bai4 {
+//    4. Cho một giá trị int x và một mảng int [] A ={.....}; gồm các phần tử tạo thành 1 dãy tăng dần;
+//Thực hiện tìm kiếm nhị phân phần tử x trong mảng A.
+
+    public static void main(String[] args) {
+        int[] A = {1, 3, 5, 7, 8, 11, 13, 17, 19};
+        int x = 8;
+        
+    }
+}
