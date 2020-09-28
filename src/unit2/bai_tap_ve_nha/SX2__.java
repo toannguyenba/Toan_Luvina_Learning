e6ce9657d1b5c530be42cb783b3981e380e37b09
Nguyen Ba Toan <ngabatoan22@gmail.com>
2020-09-28 08:50:07 +0700

    sua bai 2.2

--- src/unit2/bai_tap_ve_nha/SX2__.java
+++ src/unit2/bai_tap_ve_nha/SX2__.java
@@ -1,23 +1,22 @@
 package unit2.bai_tap_ve_nha;
 
 public class SX2__ {
-	public static void main(String[] args) {
-		int[] A = { 17, 21, 9, 12, 6, 18, 4, 20, 7, 14 };
-		for (int i = A.length - 2; i >= 0; i--) {
-			int temp = A[i];
-			for (int j = i + 1; j < A.length; j++) {
-				if (temp > A[j])
-					A[j - 1] = A[j];
-				else {
-					A[j - 1] = temp;
-					break;
-				}
-				if (j == A.length - 1)
-					A[j] = temp;
-			}
-		}
-		for (int number : A) {
-			System.out.print(number + " ");
-		}
-	}
+
+    public static void main(String[] args) {
+        int[] A = {17, 21, 9, 12, 6, 18, 4, 20, 7, 14};
+        for (int i = A.length - 2; i >= 0; i--) {
+            int temp = A[i];
+            for (int j = i; j < A.length - 1; j++) {
+                if (temp > A[j + 1]) {
+                    A[j] = A[j + 1];
+                } else {
+                    break;
+                }
+                A[j + 1] = temp;
+            }
+        }
+        for (int number : A) {
+            System.out.print(number + " ");
+        }
+    }
 }
